05d90a12e245d17284f7b32b501742de80c4f025
Vicsante Tangpos Aseniero <vaseniero@gmail.com>
2019-03-18 07:48:05 +0800

    changes on app display and added functionalities

--- app/src/main/java/com/example/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/example/firstandroidapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.firstandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Show a toast
+     * @param view -- the view that is clicked
+     */
+    public void toastMe(View view) {
+        // Toast myToast = Toast.makeText(this, message, duration);
+        Toast myToast = Toast.makeText(this, "Hello Toast", Toast.LENGTH_SHORT);
+        myToast.show();
     }
 }
